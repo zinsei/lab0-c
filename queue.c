2580fced8e3c29457f4cbda10a0616f4e477b9c1
Eugene Jeng <sonorads@gmail.com>
2025-03-09 20:12:32 +0800

    Implement queue create, remove, insert and free
    q_new() allocates a list_head size space,
    and link sets both @next and @prev to point to the structure itself.
    
    q_free() uses list_for_each_entry_safe() to traverse all nodes from
    designated @head, and free all allocated @elements from element_t.
    
    q_insert_head() allocates a element_t size space,
    and copy designated character into element_t, add this element_t after
    designated @head.
    
    q_insert_tail() allocates a element_t size space,
    and copy designated character into element_t, add this element_t at the
    tail of designated @head.
    
    q_remove_head() uses list_first_entry() to find first element_t,
    and copy buffer size length character to designated character, and
    delete the finding element_t.
    
    q_remove_tail() uses list_last_entry() to find last element_t,
    and copy buffer size length character to designated character, and
    delete the finding element_t.
    
    q_delete_mid() uses fast slow pointer to find the middle of @head
    pointer in the list, and delete finding node.
    
    Change-Id: I1c83e8717ae1ec5ababaa43084b5231975502d00

--- queue.c
+++ queue.c
@@ -7,34 +7,102 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+
+    INIT_LIST_HEAD(head);
+
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *iter = NULL;
+    element_t *safe = NULL;
+    list_for_each_entry_safe (iter, safe, head, list) {
+        if (iter->value)
+            free(iter->value);
+        list_del(&iter->list);
+        free(iter);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+    list_add(&element->list, head);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+    list_add_tail(&element->list, head);
+
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *element = list_first_entry(head, element_t, list);
+    if (sp && element->value) {
+        strncpy(sp, element->value, bufsize);
+        element->value = NULL;
+    }
+    list_del(&element->list);
+
+    return element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *element = list_last_entry(head, element_t, list);
+    if (sp && element->value) {
+        strncpy(sp, element->value, bufsize);
+        element->value = NULL;
+    }
+    list_del(&element->list);
+
+    return element;
 }
 
 /* Return number of elements in queue */
@@ -56,6 +124,21 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || !head->next)
+        return false;
+
+    struct list_head *prev = NULL;
+    struct list_head *slow = head;
+    struct list_head *fast = head->next;
+
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    list_del(slow);
+
     return true;
 }
 
